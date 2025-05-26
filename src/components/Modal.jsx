40f9b622f656b8baad8d78976bdd0d0403a24d78
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-25 23:20:38 -0400

    created modal and sidebar components

---
+++ src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import { FaTimes } from 'react-icons/fa'
+
+function Modal() {
+    return (
+        <div>
+            Modal
+        </div>
+    )
+}
+
+export default Modal;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { FaTimes } from 'react-icons/fa';
+
+function Sidebar() {
+    return (
+        <div>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar;
