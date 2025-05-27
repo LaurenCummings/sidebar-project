72ab5276dee945eb985fb9f783461003a84eaa48
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-26 21:04:55 -0400

    imported home, modal, and sidebar components into app and displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Home from './components/Home';
+import Modal from './components/Modal';
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
     <div>
-      App
+      <Home />
+      <Modal />
+      <Sidebar />
     </div>
   )
 }
