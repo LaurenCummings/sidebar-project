da43c988570d40e4388868c6d1c43b567f475f66
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-26 23:16:01 -0400

    Created functions for opening and closing sidebar and modal

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,6 +3,25 @@ import { useState, useContext, createContext } from 'react';
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true);
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false);
+    }
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    }
+
     return (
         <AppContext.Provider value="hello">
             {children}
